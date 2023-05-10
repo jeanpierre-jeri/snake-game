be50ea4cf8c4a62ecb34cdd551a1a24373474099
Jean Pierre <jeanpierre.webdev@gmail.com>
2023-05-10 00:15:59 -0500

    feat: set board size constant to 30

--- src/config/contants.ts
+++ src/config/contants.ts
@@ -1,4 +1,4 @@
-export const BOARD_SIZE = 5
+export const BOARD_SIZE = 30
 
 export const INITIAL_SNAKE_POSITION_X = Math.floor(BOARD_SIZE / 2)
 export const INITIAL_SNAKE_POSITION_Y = Math.floor(BOARD_SIZE / 2)
