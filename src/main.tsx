5d55432fca96b9402ef96ebc4fb5252d5bc4a62e
Jean Pierre <jeanpierre.webdev@gmail.com>
2023-05-10 09:17:05 -0500

    feat: import directly dark mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import './sweetalert.scss'
+import '@sweetalert2/theme-dark/dark.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <App />
